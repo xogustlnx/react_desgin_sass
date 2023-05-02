560b21178ee18adb31f98913294029e7cb788535
Kiko <sthyeon11#gmail.com>
2023-05-02 15:31:57 +0000

    use_rest_props

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,12 @@ import React from "react";
 import classNames from "classnames";
 import "./Button.scss";
 
-const Button = ({ children, size, color, outline, full}) => {
+const Button = ({ children, size, color, outline, full, ...rest }) => {
   return (
-    <button className={classNames("Button", size, color, { outline }, {full})}>
+    <button
+      className={classNames("Button", size, color, { outline }, { full })}
+      {...rest}
+    >
       {children}
     </button>
   );
